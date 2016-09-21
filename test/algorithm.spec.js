eb5d69b8682f6ccedff15dc7a9a54da290010d85
Renee <rchar20@gmail.com>
2016-09-21 09:44:11 -1000

    adds a new test when number is 300

--- test/algorithm.spec.js
+++ test/algorithm.spec.js
@@ -42,6 +42,7 @@ describe('Algorithm', () => {
 		it('should return the sum of the square roots up until the number passed in to the nearest hundreth digit', () => {
 			expect(myAlgorithm.sumOfSquareRoots(2)).to.equal(2.41);
 			expect(myAlgorithm.sumOfSquareRoots(5)).to.equal(8.38);
+			expect(myAlgorithm.sumOfSquareRoots(300)).to.equal(3472.53);
 		});
 		
 	});
